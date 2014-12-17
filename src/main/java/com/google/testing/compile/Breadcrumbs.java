068f496137ab35993858eeb5bed154cfc532fd4f
dingdongsilver <42iseverything@gmail.com>
2014-12-17 17:26:04 -0500

    WUT AM I DOING

--- src/main/java/com/google/testing/compile/Breadcrumbs.java
+++ src/main/java/com/google/testing/compile/Breadcrumbs.java
@@ -12,7 +12,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+ * Well fuck me in the ass.....
+*/
+
 package com.google.testing.compile;
 
 import com.google.common.base.Function;
